d50bd2199dddfca775adeadd49784ee241389624
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-02-21 23:32:34 +0100

    fixing home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import './Home.scss'
-const url = "https://previews.123rf.com/images/pe3check/pe3check1711/pe3check171100493/90431368-sports-news-on-blue-dotted-map-background-illustration-vector.jpg"
 
 const Home = () => {
   return (
     <div className="home_container">
-        <img src={url}/>
+        <div className="title">
+        <h1>SportsNews</h1>
+        </div>
+        <div className="suscription">
+        <button>Suscribe to our newsletter!</button>
+        </div>
     </div>
     
   )
